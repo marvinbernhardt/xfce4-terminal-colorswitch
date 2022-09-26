988aed732f064211fbc621ebec764e79aa9d4164
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2022-09-26 21:52:41 +0200

    remove taskwarrior, make polybar config file read only

--- colorswitch.py
+++ colorswitch.py
@@ -8,38 +8,44 @@ from string import Template
 import tempfile
 import time
 
-DESCRIPTION = """Change the theme of xfce4 and xfce4-terminal, and taskwarrior"""
+DESCRIPTION = """Change the theme of xfce4, and xfce4-terminal, and polybar"""
 
 
 def run():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('brightness', type=str,
-                        choices=['dark', 'light'],
-                        help='choose how the desktop should look like')
+    parser.add_argument(
+        "brightness",
+        type=str,
+        choices=["dark", "light"],
+        help="choose how the desktop should look like",
+    )
 
     # set themes
     args = parser.parse_args()
-    if args.brightness == 'light':
-        xfce4_style = 'Adwaita'
-        xfce4_terminal_colorscheme = 'marv-light'
-        task_theme = 'light-256'
-        polybar_colors = {'background': '#f6f5f4', 'foreground': '#222', 'icon': '#666',
-                          'border_top_color': '#b2aca6'}
-    elif args.brightness == 'dark':
-        xfce4_style = 'Adwaita-dark'
-        xfce4_terminal_colorscheme = 'marv-dark'
-        task_theme = 'dark-256'
-        polybar_colors = {'background': '#353535', 'foreground': '#eee', 'icon': '#888',
-                          'border_top_color': '#252525'}
+    if args.brightness == "light":
+        xfce4_style = "Adwaita"
+        xfce4_terminal_colorscheme = "marv-light"
+        polybar_colors = {
+            "background": "#f6f5f4",
+            "foreground": "#222",
+            "icon": "#666",
+            "border_top_color": "#b2aca6",
+        }
+    elif args.brightness == "dark":
+        xfce4_style = "Adwaita-dark"
+        xfce4_terminal_colorscheme = "marv-dark"
+        polybar_colors = {
+            "background": "#353535",
+            "foreground": "#eee",
+            "icon": "#888",
+            "border_top_color": "#252525",
+        }
 
     # change xfce4 style
     change_xfce4_style(xfce4_style)
 
     # change xfce4-terminal colorscheme
     change_xfce4_terminal_colorscheme(xfce4_terminal_colorscheme)
-
-    # change taskwarrior theme
-    change_taskwarrior_theme(task_theme)
 
     # change polybar colors
     change_polybar_colors(polybar_colors)
@@ -50,16 +56,18 @@ def run():
 def change_xfce4_style(style):
     """changes xfce4 style"""
     # does not work with subprocess.run() for some reason
-    os.system("/usr/bin/xfconf-query -c xsettings -p /Net/ThemeName -s '{}'".format(
-        style))
+    os.system(
+        "/usr/bin/xfconf-query -c xsettings -p /Net/ThemeName -s '{}'".format(style)
+    )
     os.system("/usr/bin/xfce4-panel -r")
     os.system("/usr/bin/nohup xfwm4 --replace > /tmp/xfwm4-nohup.out &")
 
 
 def change_xfce4_terminal_colorscheme(colorscheme):
     os_file = f"/usr/share/xfce4/terminal/colorschemes/{colorscheme}.theme"
-    user_file = os.path.expanduser("~/.local/share/xfce4/terminal/"
-                                   f"colorschemes/{colorscheme}.theme")
+    user_file = os.path.expanduser(
+        "~/.local/share/xfce4/terminal/" f"colorschemes/{colorscheme}.theme"
+    )
     rc_file = os.path.expanduser("~/.config/xfce4/terminal/terminalrc")
     random_string = next(tempfile._get_candidate_names())
     rc_temp = os.path.expanduser(f"/tmp/terminalrc_{random_string}")
@@ -79,49 +87,38 @@ def change_xfce4_terminal_colorscheme(colorscheme):
     scheme_conf = configparser.ConfigParser(interpolation=None)
     scheme_conf.optionxform = str
     scheme_conf.read(scheme_file)
-    for key, value in dict(scheme_conf['Scheme']).items():
-        if not key.startswith('Name'):
-            term_conf['Configuration'][key] = value
+    for key, value in dict(scheme_conf["Scheme"]).items():
+        if not key.startswith("Name"):
+            term_conf["Configuration"][key] = value
 
-    with open(rc_temp, 'w') as f:
+    with open(rc_temp, "w") as f:
         term_conf.write(f, space_around_delimiters=False)
 
     shutil.move(rc_temp, rc_file)
 
 
-def change_taskwarrior_theme(theme):
-    """changes theme of taskwarrior
-    ~/.taskrc-theme must be included in .taskrc"""
-
-    os_file = f"/usr/share/doc/task/rc/{theme}.theme"
-    taskrc_theme_file = os.path.expanduser("~/.taskrc-theme")
-
-    # check if scheme is available
-    if not os.path.isfile(os_file):
-        raise ValueError(f"file {os_file} not found")
-
-    with open(taskrc_theme_file, 'w') as f:
-        f.write(f"include {os_file}\n")
-
-
 def change_polybar_colors(polybar_colors):
     """changes colors in .config/polybar/config.pytemplate
-       and saves them to .config/polybar/config."""
+    and saves them to .config/polybar/config."""
     template_file = os.path.expanduser("~/.config/polybar/config.pytemplate")
     config_file = os.path.expanduser("~/.polybar-config")
     random_string = next(tempfile._get_candidate_names())
     temp_file = os.path.expanduser(f"/tmp/polybar-config-{random_string}")
     # open template file
-    with open(template_file, 'r') as f:
+    with open(template_file, "r") as f:
         config = f.read()
     # apply config
     config = Template(config).substitute(polybar_colors)
     # write config to temp file
-    with open(temp_file, 'w') as f:
+    with open(temp_file, "w") as f:
         f.write(config)
+    # make writable
+    os.chmod(config_file, 0o600)
     # atomic change
     shutil.move(temp_file, config_file)
+    # read only
+    os.chmod(config_file, 0o400)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
